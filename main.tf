aadb95ce539ca6a204cd362ea668b6b7bea26de6
derisiano <andi.stein@gmail.com>
2026-04-23 02:25:36 -0400

    23 abril 26

---
+++ main.tf
@@ -0,0 +1,12 @@
+####### provider #######
+provider "aws" { 
+    region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+#### resource #####
+resource "aws_instance" "nginx-server" { 
+    ami = "ami-04403b780d96b29d"
+    instance_type = "t3.micro"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+secret_key = "ykdb3jd83S+"
+access_key = "plbhE4+2S3l"
